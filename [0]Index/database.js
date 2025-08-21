8f4bb18a572cf7504a879472d43676d81dc3603b
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-08-21 20:38:02 +0200

    update

--- [0]Index/database.js
+++ [0]Index/database.js
@@ -1,17 +1,21 @@
 const websites = [
-    
+
 ];
 const agenda = [
     "Tinderella",
     "Puente",
     "Earthserver",
-    "Mecanicas + Teyvat",    
-"webtrabajo",
-"laligapro",
-"fotografias viaje",
-"europa",
-"cuadernos de cine",
-"cuadernos de articulos periodico",
-"correo gmail",
-"cuenta de drive"
+    "Mecanicas + Teyvat",
+    "webtrabajo",
+    "laligapro",
+    "fotografias viaje",
+    "europa",
+    "cuadernos de cine",
+    "cuadernos de articulos periodico",
+    "correo gmail",
+    "cuenta de drive",
+    "fotoperiodismo website",
+    "cocemfe",
+    "labora tutora",
+    "cuaderno de copper"
 ];
