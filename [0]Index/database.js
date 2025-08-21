c48b8b7f3fc1a3bf686a61f07a20e74db86cf94c
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-08-21 11:22:45 +0200

    update index main

---
+++ [0]Index/database.js
@@ -0,0 +1,14 @@
+const websites = [
+    "ClocksWorld",
+    "FlickMovies",
+    "LazyTraductor",
+    "LearningSQL",
+    "Valencia 2030"
+];
+const agenda = [
+    "Tinderella",
+    "Puente",
+    "Earthserver",
+    "Mecanicas + Teyvat",
+    
+];
