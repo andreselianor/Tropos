8d1b15b9081ed1b21ed3e8becc71c991e0ec9f54
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-09-07 12:24:31 +0200

    Actualizar database.js

--- Index/mainScripts/database.js
+++ Index/mainScripts/database.js
@@ -1,113 +1,114 @@
 const agenda = {
-    Cromos: {
-        title: "Cromos",
-        description: "Proyecto de cromos 3D"
-    },
-    FlickMovies: {
-        title: "FlickMovies",
-        description: "Completar el resto de categorias. Añadir imagenes a las categorias."
-    },
-    ITInstitute: {
-        title: "ITInstitute",
-        description: "Mash up entre música de baile y música clásica."
-    },
-    Urbanismo: {
-        title: "Urbanismo",
-        description: "En planeamiento sector 'Jardín Villa Diagonal '."
-    }, Decoracion: {
-        title: "Decoracion",
-        description: "Comprar mantel y medidas."
-    },
-    Viajes: {
-        title: "Viajes",
-        description: "Preparacion de viajes a distintos destinos: Novelda, La Nucia, Cuenca, Madrid."
-    },
-    Antkind: {
-        title: "Antkind",
-        description: "Puesta a punto del proyecto de recuperación y lectura de libros. Recuperación de webapp."
-    },
-    SteamCollective: {
-        title: "Steam Collective",
-        description: "Preparación del proyecto."
-    },
-    CodeLearning: {
-        title: "Code Learning",
-        description: "Continuar estudiando JAVA, Phyton y C# desde 'exercism'."
-    },
-    EveonlineTshirt: {
-        title: "Eveonline Tshirt",
-        description: "Imprimir imagenes. Llevar imagenes en un pendrive."
-    },
-    SmartElectronics: {
-        title: "SmartElectronics",
-        description: "Compra en Tienda online de productos electrónicos: Breadboard, leds, cables y pilas."
-    },
-    Tinderella: {
-        title: "Tinderella",
-        description: "Revisar aplicación móvil."
-    },
-    Puente: {
-        title: "Puente",
-        description: "Completar las protecciones de las pilas del puente."
-    },
-    Mecanicas: {
-        title: "Mecanicas",
-        description: "Puesta a punto de las distintas mecánicas."
-    },
-    Teyvat: {
-        title: "Teyvat",
-        description: "Cierre de categorías de Teyvat."
-    },
-    LaLigaPro: {
-        title: "LaLigaPro",
-        description: "Imprimir cromos faltantes para completar la colección."
-    },
-    FotografiasViaje: {
-        title: "Fotografias Viaje",
-        description: "Terminar el producto de anillas."
-    },
-    Periodismo: {
-        title: "Periodismo",
-        description: "Continuar con los recortes de periódico."
-    },
-    Gmail: {
-        title: "Gmail",
-        description: "Ordenar todas la información que llega por email."
-    },
-    Cocemfe: {
-        title: "Cocemfe",
-        description: "Completar la relación con Cocemfe."
-    },
-    Labora: {
-        title: "Labora",
-        description: "Entrevista con la orientadora."
-    },
-    Trabajo: {
-        title: "Trabajo",
-        description: "Actualización de redes de búsqueda de empleo."
-    },
-    Copperplate: {
-        title: "Copperplate",
-        description: "Almacenar proyecto."
-    },
-    HalleCollection: {
-        title: "HalleCollection",
-        description: "Diseño del proyecto de catálogo de películas."
-    },
-    Adultos: {
-        title: "Adultos",
-        description: "Poner a prueba la aplicación."
-    },
-    EcosEsen: {
-        title: "EcosEsen",
-        description: "Recuperar documentos."
-    },
-    Webscan: {
-        title: "Webscan",
-        description: "Diseño de proyecto de webscan."
-    },
-    Bookmarks: {
-        title: "Bookmarks",
-        description: "Pagina web con marcadores."
-    }
+  Periodismo: {
+    title: "Periodismo",
+    description: "Continuar con los recortes de periódico."
+  },
+  Google: {
+    title: "Google",
+    description: "Gestionar productos Google."
+  },
+  Imprimir: {
+    title: "Imprimir",
+    description: "laliga + eveonline"
+  },
+  LaLigaPro: {
+    title: "LaLigaPro",
+    description: "Completar album + base de datos."
+  },
+  FotografiasViaje: {
+    title: "Fotografias Viaje",
+    description: "Terminar el producto de anillas."
+  },
+  Fotografiasalbum: {
+    title: "Fotografias Album",
+    description: "Terminar el producto de album."
+  },
+  Decoracion: {
+    title: "Decoracion",
+    description: "Comprar mantel y medidas."
+  },
+  FlickMovies: {
+    title: "FlickMovies",
+    description: "Completar el resto de categorias. Añadir imagenes a las categorias."
+  },
+  ITInstitute: {
+    title: "ITInstitute",
+    description: "Mash up entre música de baile y música clásica."
+  },
+  EveonlineTshirt: {
+    title: "Eveonline Tshirt",
+    description: "Fabricar camiseta."
+  },
+  SmartElectronics: {
+    title: "SmartElectronics",
+    description: "Compra en Tienda online de productos electrónicos: Breadboard, leds, cables y pilas."
+  },
+  Tinderella: {
+    title: "Tinderella",
+    description: "Factorizacion de codigo fuente.."
+  },
+  Bookmarks: {
+    title: "Bookmarks",
+    description: "Pagina web con marcadores."
+  },
+  Cocemfe: {
+    title: "Cocemfe",
+    description: "Completar la relación con Cocemfe."
+  },
+  Labora: {
+    title: "Labora",
+    description: "Entrevista con la orientadora."
+  },
+  Trabajo: {
+    title: "Trabajo",
+    description: "Actualización de redes de búsqueda de empleo."
+  },
+  CromosSteam: {
+    title: "Cromos Steam",
+    description: "Proyecto de cromos 3D sobre productos Steam."
+  },
+  SteamLegacy: {
+    title: "Steam Legacy",
+    description: "Preparación de las bases."
+  },
+  Urbanismo: {
+    title: "Urbanismo",
+    description: "En planeamiento sector 'Jardín Villa Diagonal'."
+  },
+  CodeLearning: {
+    title: "Code Learning",
+    description: "Continuar estudiando JAVA, Phyton y C# desde 'exercism'."
+  },
+  Copperplate: {
+    title: "Copperplate",
+    description: "Preparar proyecto."
+  },
+  HalleCollection: {
+    title: "HalleCollection",
+    description: "Diseño del proyecto de catálogo de películas."
+  },
+  Puente: {
+    title: "Puente",
+    description: "Completar las protecciones de las pilas del puente."
+  },
+  Adultos: {
+    title: "Adultos",
+    description: "Poner a prueba la aplicación."
+  },
+  EcosEsen: {
+    title: "EcosEsen",
+    description: "Recuperar documentos."
+  },
+  Webscan: {
+    title: "Webscan",
+    description: "Diseño de proyecto de webscan."
+  },
+  Antkind: {
+    title: "Antkind",
+    description: "Puesta a punto del proyecto de recuperación y lectura de libros. Recuperación de webapp."
+  },
+  Viajes: {
+    title: "Viajes",
+    description: "Preparacion de viajes a distintos destinos: Novelda, La Nucia, Cuenca, Madrid."
+  }
 }
