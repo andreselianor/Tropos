8f980002b28aa891edfeee88f16d88729135ee42
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-09-23 14:29:48 +0200

    add pictures / improve source code with new commission

--- TheCommission/TheCommission/mainScripts/mainJs.js
+++ TheCommission/TheCommission/mainScripts/mainJs.js
@@ -1,28 +1,44 @@
-document.addEventListener("DOMContentLoaded", start);
-const picture_array = [
-    "./TheCommission/mainResources/pictures/2025-08-06.png",
-    "./TheCommission/mainResources/pictures/2025-08-10.png",
-    "./TheCommission/mainResources/pictures/2025-08-14.png",
-    "./TheCommission/mainResources/pictures/2025-08-30.png",
-    "./TheCommission/mainResources/pictures/2025-08-31.png",
-    "./TheCommission/mainResources/pictures/2025-09-02.png",
-    "./TheCommission/mainResources/pictures/2025-09-12.png",
-    "./TheCommission/mainResources/pictures/2025-09-17.png"
-]
+/*/ SCRIPT FOR EUROPEAN COMMISSION /*/
+const pictures = {
+    foreign: [
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-08-06.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-08-10.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-08-14.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-08-30.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-08-31.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-09-02.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-09-12.png",
+        "./TheCommission/mainResources/commissioners/foreign/pictures/2025-09-17.png"
+    ],
+    mediterranean: [
+        "./TheCommission/mainResources/commissioners/mediterranean/pictures/2025-09-23.png"
+    ]
+}
 
-function start() {
+window.onload = function () {
+    display('foreign');
+}
+
+function display(commission) {
     let counter = 0;
     let picture = document.getElementsByClassName("picture")[0];
     let text = document.getElementsByClassName("paragraph")[0];
     let picture_event = document.getElementById("picture_container");
+
+    picture.src = pictures[commission][0];
+    if (commission == "mediterranean")
+        text.textContent = "Dubravka Šuica";
+    else if (commission == "foreign")
+        text.textContent = pictures[commission][0].substring(61, 71);
+
     picture_event.addEventListener('click', function () {
         counter++;
         if (counter < 0)
             counter = 0;
-        if (counter >= picture_array.length)
+        if (counter >= pictures[commission].length)
             counter = 0;
-        picture.setAttribute('src', picture_array[counter]);
-        text.textContent = picture_array[counter].substring(39, 49);
+        picture.src = pictures[commission][counter];
+        text.textContent = pictures[commission][counter].substring(61, 71);
     })
 }
 
