d8052216cb101e3fb0251798074e9cc9e0685322
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-10-07 22:52:08 +0200

    version 1.0.0

--- WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
+++ WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
@@ -6,11 +6,12 @@ const URL_Facebook = "https://www.facebook.com";
 const URL_Instagram = "https://www.instagram.com";
 const URL_Flickr = "https://www.flickr.com";
 const URL_Pinterest = "https://www.pinterest.com";
+const URL_Deviantart = "https://www.deviantart.com";
 
 // variables
 var path = URL_Facebook;
 var logos_path = "./WebSocialBookmarks/mainResources/logos/";
-var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png"];
+var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png"];
 
 // main function
 function setup() {
@@ -25,12 +26,17 @@ function setup() {
     document.getElementById("flickr").addEventListener('click', function () {
         document.getElementById("image-container").src = logos_path + logos[2];
         path = URL_Flickr;
-    })
+    });
 
     document.getElementById("pinterest").addEventListener('click', function () {
         document.getElementById("image-container").src = logos_path + logos[3];
         path = URL_Pinterest;
-    })
+    });
+
+    document.getElementById("deviantart").addEventListener('click', function () {
+        document.getElementById("image-container").src = logos_path + logos[4];
+        path = URL_Deviantart;
+    });
 
     // main event listener
     document.getElementById("image-container").addEventListener('click', function () { window.open(path, "_blank") });
