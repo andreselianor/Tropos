45409a59f6a424b02be95bd67454d3d12b1e95a9
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-10-09 15:10:28 +0200

    update new website - vinted

--- WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
+++ WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
@@ -7,11 +7,12 @@ const URL_Instagram = "https://www.instagram.com";
 const URL_Flickr = "https://www.flickr.com";
 const URL_Pinterest = "https://www.pinterest.com";
 const URL_Deviantart = "https://www.deviantart.com";
+const URL_Vinted = "https://www.vinted.es";
 
 // variables
 var path = URL_Facebook;
 var logos_path = "./WebSocialBookmarks/mainResources/logos/";
-var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png"];
+var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png", "vinted_logo.png"];
 
 // main function
 function setup() {
@@ -36,6 +37,11 @@ function setup() {
     document.getElementById("deviantart").addEventListener('click', function () {
         document.getElementById("image-container").src = logos_path + logos[4];
         path = URL_Deviantart;
+    });
+
+    document.getElementById("vinted").addEventListener('click', function () {
+        document.getElementById("image-container").src = logos_path + logos[5];
+        path = URL_Vinted;
     });
 
     // main event listener
