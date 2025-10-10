8b1e2b5f720ad8d53fbf1260cebf3763f4169b04
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-10-10 22:37:07 +0200

    update new websites

--- WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
+++ WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
@@ -8,11 +8,13 @@ const URL_Flickr = "https://www.flickr.com";
 const URL_Pinterest = "https://www.pinterest.com";
 const URL_Deviantart = "https://www.deviantart.com";
 const URL_Vinted = "https://www.vinted.es";
+const URL_Tinder = "https://www.tinder.es";
+const URL_Meetic = "https://www.meetic.com";
 
 // variables
 var path = URL_Facebook;
 var logos_path = "./WebSocialBookmarks/mainResources/logos/";
-var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png", "vinted_logo.png"];
+var logos = ["facebook_logo.png", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png", "vinted_logo.png", "tinder_logo.png","meetic_logo.png"];
 
 // main function
 function setup() {
@@ -42,6 +44,16 @@ function setup() {
     document.getElementById("vinted").addEventListener('click', function () {
         document.getElementById("image-container").src = logos_path + logos[5];
         path = URL_Vinted;
+    });
+
+    document.getElementById("tinder").addEventListener('click', function () {
+        document.getElementById("image-container").src = logos_path + logos[6];
+        path = URL_Tinder;
+    });
+
+    document.getElementById("meetic").addEventListener('click', function () {
+        document.getElementById("image-container").src = logos_path + logos[7];
+        path = URL_Meetic;
     });
 
     // main event listener
