525ff3ac6e9427a9cb54067ddb8aad65a66ac0ee
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-10-25 23:00:20 +0200

    update web social bookmarks

--- WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
+++ WebSocialBookmarks/WebSocialBookmarks/mainScripts/mainJs.js
@@ -1,4 +1,4 @@
-/*/ MAIN SCRIPT FOR SOCIALMEDIABOOKMARKS /*/
+/*/ MAIN SCRIPT FOR SOCIAL MEDIA BOOKMARKS /*/
 window.onload = setup;
 
 // urls
@@ -7,6 +7,7 @@ const URL_Flickr = "https://www.flickr.com";
 const URL_Pinterest = "https://www.pinterest.com";
 const URL_Deviantart = "https://www.deviantart.com";
 const URL_Vinted = "https://www.vinted.es";
+const URL_Zalando = "https://www.zalando.es";
 const URL_Tinder = "https://www.tinder.com";
 const URL_Meetic = "https://www.meetic.com";
 const URL_Badoo = "https://www.badoo.com";
@@ -14,7 +15,7 @@ const URL_Badoo = "https://www.badoo.com";
 // variables
 var path = URL_Instagram;
 var logos_path = "./WebSocialBookmarks/mainResources/logos/";
-var logos = ["-","instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png", "vinted_logo.png", "tinder_logo.png","meetic_logo.png","badoo_logo.png"];
+var logos = ["-", "instagram_logo.png", "flickr_logo.png", "pinterest_logo.png", "deviantart_logo.png", "vinted_logo.png", "zalando_logo.png", "tinder_logo.png", "meetic_logo.png", "badoo_logo.png"];
 
 // main function
 function setup() {
@@ -42,18 +43,23 @@ function setup() {
         path = URL_Vinted;
     });
 
-    document.getElementById("tinder").addEventListener('click', function () {
+    document.getElementById("zalando").addEventListener('click', function () {
         document.getElementById("image-container").src = logos_path + logos[6];
+        path = URL_Zalando;
+    });
+
+    document.getElementById("tinder").addEventListener('click', function () {
+        document.getElementById("image-container").src = logos_path + logos[7];
         path = URL_Tinder;
     });
 
     document.getElementById("meetic").addEventListener('click', function () {
-        document.getElementById("image-container").src = logos_path + logos[7];
+        document.getElementById("image-container").src = logos_path + logos[8];
         path = URL_Meetic;
     });
 
     document.getElementById("badoo").addEventListener('click', function () {
-        document.getElementById("image-container").src = logos_path + logos[8];
+        document.getElementById("image-container").src = logos_path + logos[9];
         path = URL_Badoo;
     });
 
