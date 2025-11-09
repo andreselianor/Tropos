ae7ba5351434a08fedb0504844cdf0d797d9a1c4
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-11-09 19:00:39 +0100

    update new week task

--- 0_Index/mainScripts/database.js
+++ 0_Index/mainScripts/database.js
@@ -1,81 +1,15 @@
 const agenda = {
-  Trabajo: {
-    title: "Trabajo",
+  Agenda: {
+    title: "Agenda",
     description: "",
     weight: "bold"
-  },
-  Redes: {
-    title: "Job Finden",
-    description: "Actualizar todos los CV"
-  },
-  Gestion: {
-    title: "Gestion",
-    weight: "bold"
-  },
-  Salud: {
-    title: "Salud",
-    description: "",
-    weight: "bold"
-  },
-  Analitica: {
-    title: "Analitica",
-    description: "Centro de salud"
-  },
-  Creatividad: {
-    title: "Creatividad",
-    weight: "bold"
-  },
-  Expositor: {
-    title: "Expositor",
-    description: "Decoración para exponer figurillas."
-  },
-  Ciudad: {
-    title: "Ciudad",
-    weight: "bold"
-  },
-  Personales: {
-    title: "Personales",
-    weight: "bold"
-  },
-  CodeLearning: {
-    title: "Code Learning",
-    description: "Continuar estudiando JAVA, Phyton y C# desde 'exercism'."
-  },
-  SqlLearning: {
-    title: "SQL learning",
-    description: "Aprender base de datos."
   }
 }
 
 const projects = {
-  AirBNB: {
-    title: "AirBNB",
-    description: "Actualizacion de perfil",
+  Projects: {
+    title: "Projects",
+    description: "Descripcion del proyecto",
     decoration : "strike"
-  },
-  Medicinas: {
-    title: "Guia de medicinas",
-    description: "Comprobar medicinas",
-    decoration: "strike"
-  },
-  Google: {
-    title: "Google",
-    description: "Servicios en la nube"
-  },
-  Seguridad: {
-    title: "Seguridad",
-    description: "Actualizacion de seguridad"
-  },
-  Trabajo: {
-    title: "Trabajo",
-    description: "Cursos y perfiles"
-  },
-  Fotoprix: {
-    title: "Fotoprix",
-    description: "Salir de las fotografias"
-  },
-  Alimentacion: {
-    title: "Fichas salud",
-    description: "Crear carpetas / empezar dieta"
   }
 }
