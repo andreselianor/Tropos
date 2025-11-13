dbe07c63f0b08981a80d35beeb485baa4e8e89c5
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-11-13 17:58:09 +0100

    new agenda

--- 0_Index/mainScripts/database.js
+++ 0_Index/mainScripts/database.js
@@ -3,6 +3,22 @@ const agenda = {
     title: "Agenda",
     description: "",
     weight: "bold"
+  },
+  Fotoprix: {
+    title: "Salir de fotoprix",
+    description: "Imprimir foto"
+  },
+  AuraTrabajo: {
+    title: "Llamar a Javi",
+    description: "Preguntar por ofertas de empleo"
+  },
+  Bancos: {
+    title: "Seguridad",
+    description: "Seguridad en bancos"
+  },
+  CorteIngles: {
+    title: "Compras",
+    description: "Ofertas en portatiles"
   }
 }
 
